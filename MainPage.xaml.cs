f4760f019aec1c2a6afb2f8ebc19cefa84c6d6c6
Alloyd <71263641+Alloyd031@users.noreply.github.com>
2023-04-24 15:29:34 +0300

    Add project files.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel.Core;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.ViewManagement;
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using UWPBlox.Pages;
+using UWPBlox.Dialogs;
+using Microsoft.UI.Xaml.Controls;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace UWPBlox
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            Window.Current.SetTitleBar(AppTitleBar);
+            CoreApplication.GetCurrentView().TitleBar.ExtendViewIntoTitleBar = true;
+            ApplicationViewTitleBar titleBar = ApplicationView.GetForCurrentView().TitleBar;
+            titleBar.ButtonBackgroundColor = Colors.Transparent;
+            titleBar.ButtonInactiveBackgroundColor = Colors.Transparent;
+        }
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            this.ContentFrame.Navigate(typeof(HomePage));
+        }
+        private void HomeButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.ContentFrame.Navigate(typeof(HomePage));
+        }
+        private void DiscoverButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.ContentFrame.Navigate(typeof(DiscoverPage));
+        }
+        private void AvatarButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.ContentFrame.Navigate(typeof(AvatarPage));
+        }
+        private void ConnectButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.ContentFrame.Navigate(typeof(ConnectPage));
+        }
+        private void MoreButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.ContentFrame.Navigate(typeof(MorePage));
+        }
+    }
+}
---
+++ Pages/JoiningServerPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using UWPBlox.Dialogs;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace UWPBlox.Pages
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class JoiningServerPage : Page
+    {
+        public JoiningServerPage()
+        {
+            this.InitializeComponent();
+        }
+        private async void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            Frame contentFrame = Window.Current.Content as Frame;
+            MainPage mp = contentFrame.Content as MainPage;
+            Grid grid = mp.Content as Grid;
+            Grid PagesPanel = grid.FindName("PagesPanel") as Grid;
+            Frame ContentFrame = grid.FindName("ContentFrame") as Frame;
+            PagesPanel.Visibility = Visibility.Collapsed;
+            ContentFrame.Margin = new Thickness(0, 0, 0, 0);
+
+            JoinServerProgress.IsIndeterminate = true;
+            await Task.Delay(5000);
+            ErrorDialog dialog = new ErrorDialog();
+            await dialog.ShowAsync();
+            this.Frame.GoBack();
+            PagesPanel.Visibility = Visibility.Visible;
+            ContentFrame.Margin = new Thickness(64, 0, 0, 0);
+        }
+    }
+}
