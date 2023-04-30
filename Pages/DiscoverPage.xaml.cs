78c8f2d86c0c421064451067018e8c33fd756207
Alloyd <kirillpravikov@outlook.com>
2023-04-30 14:42:31 +0300

    more changes (game info page is still empyy btw)

--- Pages/DiscoverPage.xaml.cs
+++ Pages/DiscoverPage.xaml.cs
@@ -26,5 +26,9 @@ namespace UWPBlox.Pages
         {
             this.InitializeComponent();
         }
+        private void GameInfo_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(GameInfo));
+        }
     }
 }
